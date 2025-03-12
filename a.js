f2d21b5f509f7c0a02537c66d93639d86337da7f
Snehaleksa <166499017+Snehaleksa@users.noreply.github.com>
2025-03-12 10:44:52 +0530

    first

---
+++ a.js
@@ -0,0 +1,43 @@
+let str ='hello world'
+function nameConversion(value) {
+    console.log(value.toUpperCase());
+
+ } 
+ nameConversion(str)
+
+
+function namelength(value){
+   console.log(value.length)
+}
+namelength(str)
+
+function check(value){
+    console.log(value.includes('hello'));
+    
+}
+check(str)
+
+function trim(value){
+    console.log(value.trim());
+    
+}
+trim('  hello world')
+
+function replaceWord(){
+    console.log(str.replace('world','World'));
+    
+}
+replaceWord(str)
+
+function firstCharacter(){
+    console.log(str.charAt(0));
+    
+}
+firstCharacter(str)
+
+function firstLetter(){
+    console.log(str.charAt(0).toLocaleUpperCase());
+    
+}
+firstLetter(str)
+
